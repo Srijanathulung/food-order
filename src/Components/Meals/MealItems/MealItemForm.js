e89cf184ccb602e7c2cd407b82b28f5e5184a0d7
Srijana Thulung <srijana.thulung123@gmail.com>
2021-07-12 08:01:08 +0545

    Input component and its props is added

--- src/Components/Meals/MealItems/MealItemForm.js
+++ src/Components/Meals/MealItems/MealItemForm.js
@@ -1,9 +1,18 @@
 import classes from './MealItemForm.module.css';
+import Input from '../../UI/Card/Input';
 
 const MealItemForm = () => {
     return (
-        <form className= {classes.form}>
-            <input></input>
+        <form className={classes.form}>
+            {/* // {...props.input} so the property of input is passes In Input component */}
+            <Input label='Amount' input={{
+                id: 'amount',
+                type: 'number',
+                min: '1',
+                max: '5',
+                // step: '1',
+                defaultValue: '1'
+            }}/>
             <button> + ADD</button>
         </form>
     )
