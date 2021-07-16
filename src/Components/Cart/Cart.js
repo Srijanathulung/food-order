371312a9241685f0ec8deae776d7713d03e0ea1d
Srijana Thulung <srijana.thulung123@gmail.com>
2021-07-16 11:24:09 +0545

    displaying added item to cart

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 import classes from './Cart.module.css';
 import Modal from '../UI/Card/Modal';
 import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 const demoItems = [{ id: 'c1', name: 'burger', amount: 2, currency: 'Rs', price: 55.00 }]
 
@@ -14,10 +15,20 @@ const Cart = (props) => {
     const totalAmount =  'Rs ' + cartCtx.totalAmount.toFixed(2);
     const hasItems = cartCtx.items.length > 0;
 
+    const cartItemRemoveHandler = id => { };
+    const cartItemAddHandler = item => { };
+
     const cartItems = (
         <ul className={classes['cart-items']}>
             {cartCtx.items.map((item) => (
-                <li>{item.name}</li>
+                <CartItem
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null,item.id)}
+                    onAdd={cartItemAddHandler.bind(null,item)}
+                /> 
             ))}
         </ul>
     )
